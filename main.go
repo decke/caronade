465dc6f6f1177edab98905ae17a1d1dc43532b8f
Bernhard Froehlich <decke@bluelife.at>
2019-02-13 11:29:10 +0000

    Implement Gitea webhook parsing and introduce a waitgroup to fix logging

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -8,13 +9,16 @@ import (
 	"net/http"
 	"os/exec"
 	"path"
+	"sync"
 	"time"
 )
 
 type controller struct {
+	wg      *sync.WaitGroup
 	Workdir string
 	Logdir  string
 	Host    string
+	Secret  string
 }
 
 type worker struct {
@@ -23,11 +27,21 @@ type worker struct {
 	Status string
 }
 
+type giteaPushEventData struct {
+	Secret string `json:"secret"`
+	CommitID string `json:"after"`
+	Repository struct {
+		URL string `json:"clone_url"`
+	} `json:"repository"`
+}
+
 func newWorkerID() string {
 	return time.Now().Format("20060102150405.000000")
 }
 
 func (c *controller) startWorker(workChan chan worker) {
+	defer c.wg.Done()
+
 	for {
 		select {
 		case wrk := <-workChan:
@@ -45,6 +59,8 @@ func (c *controller) startWorker(workChan chan worker) {
 }
 
 func (c *controller) startWebhook(workChan chan worker) {
+	defer c.wg.Done()
+
 	fs := http.FileServer(http.Dir("logs"))
 	http.Handle("/logs/", http.StripPrefix("/logs/", fs))
 
@@ -52,8 +68,30 @@ func (c *controller) startWebhook(workChan chan worker) {
 		if r.Method == "GET" {
 			fmt.Fprint(w, "Welcome to caronade")
 		} else {
+			payload, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, "Internal Error", http.StatusInternalServerError)
+				return
+			}
+
+			if r.Header.Get("X-Gitea-Event") != "push" {
+				http.Error(w, "Invalid webhook", http.StatusBadRequest)
+				return
+			}
+
+			data := giteaPushEventData{}
+			if err = json.Unmarshal(payload, &data); err != nil {
+				http.Error(w, "Failed to parse webhook data", http.StatusBadRequest)
+				return
+			}
+
+			if data.Secret != c.Secret {
+				http.Error(w, "Invalid secret", http.StatusBadRequest)
+				return
+			}
+
 			select {
-			case workChan <- worker{ID: newWorkerID(), URL: "your-url", Status: "init"}:
+			case workChan <- worker{ID: data.CommitID, URL: data.Repository.URL, Status: "init"}:
 				fmt.Fprint(w, "Build started")
 				return
 			default:
@@ -71,20 +109,29 @@ func main() {
 	var workdir string
 	var logdir string
 	var host string
+	var secret string
 
 	flag.StringVar(&workdir, "workdir", "work", "Working directory")
 	flag.StringVar(&logdir, "logdir", "logs", "Buildlogs directory")
 	flag.StringVar(&host, "host", ":3000", "Interface and port to listen on")
+	flag.StringVar(&secret, "secret", "", "Webhook secret")
 	flag.Parse()
+
+	wg := sync.WaitGroup{}
 
 	ctrl := controller{
 		Workdir: workdir,
 		Logdir:  logdir,
 		Host:    host,
+		Secret:  secret,
 	}
 
 	workChannel := make(chan worker, 1)
 
+	wg.Add(2)
+
 	go ctrl.startWorker(workChannel)
-	ctrl.startWebhook(workChannel)
+	go ctrl.startWebhook(workChannel)
+
+	wg.Wait()
 }

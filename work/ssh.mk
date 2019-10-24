d5ab875c386858fdd70871d87c2de2327d05f0fc
Bernhard Froehlich <decke@bluelife.at>
2019-10-24 20:13:10 +0000

    Improve documentation with examples for a remote builder setup

--- work/ssh.mk
+++ work/ssh.mk
@@ -2,12 +2,45 @@
 # Caronade worker Makefile using SSH
 #
 
+# Example caronade.yaml:
+#
+# queues:
+# - name: 12.1/amd64
+#  recipe: ssh
+#  environment:
+#    SSH_URL: your-builder
+#    SSH_DIR: /usr/local/caronade/work/121amd64
+#    SSH_RECIPE: /usr/local/caronade/work/poudriere.mk
+#    JAIL_NAME: 121amd64
+#    PORTSTREE: 121amd64
+#
+# - name: portlint
+#  recipe: ssh
+#  environment:
+#    SSH_URL: your-builder
+#    SSH_DIR: /usr/local/caronade/work/portlint
+#    SSH_RECIPE: /usr/local/caronade/work/portlint.mk
+#    PORTSDIR: /usr/local/poudriere/ports/default
+#
+# Example .ssh/config:
+#
+# Host your-builder
+#  User root
+#  HostName the.real-hostnam.com
+#  IdentityFile ~/.ssh/id_rsa
+#  IdentitiesOnly=yes
+#  SendEnv JOB_ID COMMIT_ID REPO_URL JOB_PORT JAIL_NAME PORTSTREE PORTSDIR
+#
+# Example /etc/ssh/sshd_config on your-builder:
+#
+# AcceptEnv JOB_ID COMMIT_ID REPO_URL JOB_PORT JAIL_NAME PORTSTREE PORTSDIR
+
 SSH_URL?=	nonexistent
 SSH_DIR?=	/dev/null
 
 all: remote
 
 remote:
-	ssh ${SSH_URL} -t '/bin/bash -l -c "make -C ${SSH_DIR}"'
+	ssh ${SSH_URL} -t '/bin/sh -c "make -C ${SSH_DIR} -f ${SSH_RECIPE}"'
 
 .PHONY: remote
